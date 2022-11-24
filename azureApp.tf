29746d21593dde918e0356e4d58de28bbc8b8579
Jhansi1910 <112632902+Jhansi1910@users.noreply.github.com>
2022-11-24 21:03:00 +0530

    Update azureApp.tf

--- azureApp.tf
+++ azureApp.tf
@@ -14,19 +14,19 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "exampleRG1"
+  name     = "jhansi-1"
   location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
+  name                = "jhansi-vnet"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
+  name                 = "jhansi-subnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
@@ -41,7 +41,7 @@ resource "azurerm_subnet" "example" {
 }
 
 resource "azurerm_app_service_environment_v3" "example" {
-  name                = "example-asev3"
+  name                = "jhansi-asev3"
   resource_group_name = azurerm_resource_group.example.name
   subnet_id           = azurerm_subnet.example.id
 
